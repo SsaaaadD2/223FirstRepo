add6c26ea11d95bef75cb48d9cb0c337a8b41ed7
unknown <saad.merchant@mail.mcgill.ca>
2021-02-12 12:29:54 -0500

    Add folder for Kotlin Coroutines course

---
+++ KotlinCoroutines/app/src/main/java/com/saadm/kotlincoroutines/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.saadm.kotlincoroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+    val tag = "NewTask";
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*
+        GlobalScope.launch{
+           val networkCall = doNetworkCall();
+           Log.d(tag, networkCall);
+        }*/
+
+        /*
+        //Dispatch network call thread
+        GlobalScope.launch(Dispatchers.IO){
+            Log.d(tag, "Starting coroutine in ${Thread.currentThread().name}");
+            val networkCall = doNetworkCall();
+
+            //This block is now executed in the main thread
+            withContext(Dispatchers.Main){
+                Log.d(tag, "Switching coroutine to ${Thread.currentThread().name}");
+                Log.d(tag, networkCall);
+            }
+            */
+
+        /*
+        //Any delays placed inside this function will delay the entire thread
+        runBlocking{
+            //Don't need GlobalScope since we are within runBlocking scope
+            //This function runs async to runBlocking, so it will not be blocked
+            //if it is dispatched in a different thread
+            launch(Dispatchers.IO){
+
+            }
+        } */
+
+        /*
+        //Coroutines return jobs, which can be called later using join();
+        //Note that join does not start the job, the job has already started
+        val job = GlobalScope.launch{
+            repeat(5){
+                Log.d(tag, "Test...");
+                delay(1000);
+            }
+
+            //Coroutine is automatically cancelled after timeout
+            withTimeout(5){
+
+            }
+        }
+
+        runBlocking {
+            //job.join();
+
+            //Can cancel any job
+            //job.cancel();
+        }
+        }
+        */
+    }
+
+    suspend fun doNetworkCall():String{
+        delay(3000L);
+        return "Answer";
+    }
+}
