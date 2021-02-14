873d6ebae2c129bf76735aebe3f0ccc5cbcbac89
unknown <saad.merchant@mail.mcgill.ca>
2021-02-14 12:30:45 -0500

    Kotlin - nullable types

--- KotlinPractice/test.kt
+++ KotlinPractice/test.kt
@@ -1,4 +1,10 @@
 fun main(args: Array<String>){
-    val wholeNum : Int = 5;
-    println("The value of my variable is $wholeNum");
+    // val wholeNum : Int = 5;
+    // println("The value of my variable is $wholeNum");
+
+    // val userInput = readLine();
+    // println("You entered $userInput");
+
+    
 }
+
